4660e88c1ea9b9bc5413451ca87ca3239a156d12
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2008-08-28 12:43:19 +0200

    Mbox/ByteString/Lazy: two parsing fixes.

--- Mbox/ByteString/Lazy.hs
+++ Mbox/ByteString/Lazy.hs
@@ -1,18 +1,19 @@
 {-# LANGUAGE BangPatterns #-}
 module Mbox.ByteString.Lazy (parseMbox) where
 import Mbox (Mbox(..), MboxMessage(..))
-import Control.Arrow (first)
+import Control.Arrow ((***))
+import Control.Applicative ((<$>))
 import qualified Data.ByteString.Lazy.Char8 as C -- Char8 interface over Lazy ByteString's
 import Data.ByteString.Lazy (ByteString)
 import Data.Int (Int64)
 
-nextFrom :: ByteString -> Maybe (Int64, ByteString)
-nextFrom = goNextFrom 0
+nextFrom :: ByteString -> Maybe (ByteString, ByteString)
+nextFrom !orig = goNextFrom 0 orig
    where goNextFrom !count !input = do
-           off <- C.elemIndex '\n' input
-           let i' = C.drop (off + 1) input
+           off <- (+1) <$> C.elemIndex '\n' input
+           let i' = C.drop off input
            if C.take 5 i' == bFrom
-            then Just (off + count, C.drop 5 i')
+            then Just (C.take (off + count) orig, C.drop 5 i')
             else goNextFrom (off + count) i'
 
 bFrom :: ByteString
@@ -27,6 +28,6 @@ parseMbox = Mbox . go . skipFirstFrom
   where go !input =
           case nextFrom input of
             Nothing -> if C.null input then [] else [finishMboxMessageParsing input]
-            Just (off, rest) -> finishMboxMessageParsing (C.take off input) : go rest
+            Just (msg, rest) -> finishMboxMessageParsing msg : go rest
         finishMboxMessageParsing inp = MboxMessage sender time body
-            where ((sender,time),body) = first (C.break (==' ')) (C.break (=='\n') inp)
+            where ((sender,time),body) = C.break (==' ') *** C.tail $ C.break (=='\n') inp
