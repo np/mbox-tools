7b1c8d8ff0b132cea067bbff56ce05996b8a1aad
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2008-08-28 12:42:23 +0200

    Derive (Eq,Ord,Show) on Mbox and MboxMessage.

--- Mbox.hs
+++ Mbox.hs
@@ -1,7 +1,9 @@
 module Mbox (Mbox(..), MboxMessage(..), mboxCountMessages) where
 
 newtype Mbox s = Mbox { unMbox :: [MboxMessage s] }
+  deriving (Eq, Ord, Show)
 data MboxMessage s = MboxMessage { mboxMsgSender :: s, mboxMsgTime :: s, mboxMsgBody :: s }
+  deriving (Eq, Ord, Show)
 
 mboxCountMessages :: Mbox s -> Int
 mboxCountMessages = length . unMbox
