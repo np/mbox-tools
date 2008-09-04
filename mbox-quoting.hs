088138b18cd3a166250113b78278578c61d5ea1d
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2008-09-04 16:30:11 +0200

    mbox-tools: add mbox-quoting.

---
+++ mbox-quoting.hs
@@ -0,0 +1,12 @@
+import Mbox.ByteString.Lazy (fromQuoting)
+import qualified Data.ByteString.Lazy.Char8 as C (getContents,putStr)
+import Control.Applicative ((<$>))
+import System.Environment (getArgs)
+import System.IO (hPutStrLn,stderr)
+
+main = do
+  args <- getArgs
+  case args of
+    ["quot"]   -> C.putStr =<< fromQuoting (+1) <$> C.getContents
+    ["unquot"] -> C.putStr =<< fromQuoting (+(-1)) <$> C.getContents
+    _          -> hPutStrLn stderr "Usage: mbox-quoting {quot|unquot}"
