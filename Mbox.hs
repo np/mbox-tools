0913cd20cabb9574559d6efbc539af854c1c62fb
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2008-09-10 20:19:29 +0200

    Mbox: remove dead (or dummy) code.

--- Mbox.hs
+++ Mbox.hs
@@ -10,21 +10,9 @@
 --
 --------------------------------------------------------------------
 
-module Mbox (Mbox(..), MboxMessage(..), mboxCountMessages) where
+module Mbox (Mbox(..), MboxMessage(..)) where
 
 newtype Mbox s = Mbox { unMbox :: [MboxMessage s] }
   deriving (Eq, Ord, Show)
 data MboxMessage s = MboxMessage { mboxMsgSender :: s, mboxMsgTime :: s, mboxMsgBody :: s }
   deriving (Eq, Ord, Show)
-
-mboxCountMessages :: Mbox s -> Int
-mboxCountMessages = length . unMbox
-
-{-
-printMbox = concatMap printMsg
-  where printMsg ((s,t),Message flds body) =
-           "From "++s++" "++t++nl++concatMap printField flds++body++nl
-        printField f = show f ++ nl
-        nl = "\r\n"
--}
-
