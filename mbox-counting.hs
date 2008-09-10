8670a077190a0eddbd13fd4f0b0b569b95d98e3d
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2008-09-10 20:08:52 +0200

    mbox-counting: allow to count backward (for benchmarking).

--- mbox-counting.hs
+++ mbox-counting.hs
@@ -1,7 +1,13 @@
-import Mbox (Mbox(..), MboxMessage(..), mboxCountMessages)
+import Mbox (unMbox)
 import Control.Applicative ((<$>))
-import Mbox.ByteString.Lazy (parseMbox)
+import Mbox.ByteString.Lazy (parseMboxFile,Direction(..))
 import qualified Data.ByteString.Lazy as B
+import System.Environment
+import Data.List (delete)
 
 main :: IO ()
-main = print =<< (mboxCountMessages . parseMbox) <$> B.getContents
+--main = print =<< (mboxCountMessages . parseMbox) <$> B.getContents
+main = do
+  args <- getArgs
+  let (dir,args') = if "-r" `elem` args then (Backward,delete "-r" args) else (Forward,args)
+  print =<< (length . unMbox) <$> parseMboxFile dir (head args')
