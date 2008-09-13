de5eb752794eb332f083c267c97e495d2d13127c
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2008-09-13 19:18:42 +0200

    mbox-counting: shorten

--- mbox-counting.hs
+++ mbox-counting.hs
@@ -1,13 +1,6 @@
 import Mbox (unMbox)
-import Control.Applicative ((<$>))
-import Mbox.ByteString.Lazy (parseMboxFile,Direction(..))
+import Mbox.ByteString.Lazy (parseMbox)
 import qualified Data.ByteString.Lazy as B
-import System.Environment
-import Data.List (delete)
 
 main :: IO ()
---main = print =<< (mboxCountMessages . parseMbox) <$> B.getContents
-main = do
-  args <- getArgs
-  let (dir,args') = if "-r" `elem` args then (Backward,delete "-r" args) else (Forward,args)
-  print =<< (length . unMbox) <$> parseMboxFile dir (head args')
+main = (print . length . unMbox . parseMbox) =<< B.getContents
