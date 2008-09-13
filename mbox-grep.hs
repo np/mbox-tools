cb76037a7392f2c7ce97c8f077e8aa70c4b4bc80
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2008-09-13 22:05:37 +0200

    mbox-grep: option parser and backward reading.

--- mbox-grep.hs
+++ mbox-grep.hs
@@ -10,15 +10,25 @@
 --
 --------------------------------------------------------------------
 
-import Mbox (unMbox, mboxMsgBody)
-import Mbox.ByteString.Lazy (parseMbox)
-import Email (Email(..),readEmail,showEmailAsOneLinerDebug)
-import qualified Data.ByteString.Lazy as B (readFile)
+import Mbox (Mbox(..), MboxMessage(..))
+import Mbox.ByteString.Lazy (parseMboxFile, Direction(..), printMbox)
+import Email (Email(..),ShowFormat(..),fmtOpt,readEmail,putEmails)
 import System.Environment (getArgs)
 import Text.Parsec (parse)
 import Hutt.Query (evalQueryMsg,parseQuery)
 import Hutt.Types(GenMsg(..),GenDsc(..),MsgId(..),DscId(..),Query(..))
 import Data.Tree (Tree(..))
+import Control.Arrow
+import System.Console.GetOpt
+
+grepMbox :: Settings -> String -> String -> IO ()
+grepMbox opts mbox queryString = do
+  let query = either (error "malformed query") id $ parse parseQuery "<first-argument>" queryString
+  input <- parseMboxFile (dir opts) mbox
+  putEmails (fmt opts)
+    $ filter (emailMatchQuery query . fst)
+    $ map ((readEmail . mboxMsgBody) &&& id) . unMbox
+    $ input
 
 emailMatchQuery :: Query -> Email -> Bool
 emailMatchQuery query email = evalQueryMsg dsc msg query
@@ -35,10 +45,49 @@ emailMatchQuery query email = evalQueryMsg dsc msg query
 
 main :: IO ()
 main = do
-  [queryString,mbox] <- getArgs
-  let query = either (error "malformed query") id $ parse parseQuery "<first-argument>" queryString
-  input <- B.readFile mbox
-  mapM_ (putStrLn . showEmailAsOneLinerDebug)
-    $ filter (emailMatchQuery query)
-    $ map (readEmail . mboxMsgBody) . unMbox . parseMbox
-    $ input
+  args <- getArgs
+  let (flags, nonopts, errs) = getOpt Permute options args
+  let opts = foldr ($) defaultSettings flags
+  if help opts
+   then usage ""
+   else
+    case (nonopts, errs) of
+      ([],                  []) -> usage "Too few arguments"
+      ([mbox, queryString], []) -> grepMbox opts mbox queryString
+      (_,                   []) -> usage "Too many arguments"
+      _                         -> usage (concat errs)
+
+flipDir :: Settings -> Settings
+flipDir s = s { dir = opposite $ dir s }
+  where opposite Backward = Forward
+        opposite Forward  = Backward
+
+data Settings = Settings { fmt  :: ShowFormat
+                         , dir  :: Direction
+                         , help :: Bool
+                         }
+type Flag = Settings -> Settings
+
+defaultSettings :: Settings
+defaultSettings = Settings { fmt  = OneLinerDebug
+                           , dir  = Forward
+                           , help = False
+                           }
+
+setFmt :: ShowFormat -> Settings -> Settings
+setFmt f s = s { fmt = f }
+
+setHelp :: Settings -> Settings
+setHelp s = s { help = True }
+
+usage :: String -> a
+usage msg = error (msg ++ "\n" ++ usageInfo header options)
+  where header = "Usage: mbox-grep [OPTION...] <mbox-file> <query>"
+
+options :: [OptDescr Flag]
+options =
+  [ fmtOpt usage setFmt
+  , Option ['r'] ["reverse"] (NoArg flipDir) "Reverse the mbox order (latest firsts)"
+  , Option ['?'] ["help"]    (NoArg setHelp) "Show this help message"
+  ]
+
