ce0a175767dec84808689bddddde29c457380143
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2008-11-06 17:13:30 +0200

    Mbox: add accessors.

--- Mbox.hs
+++ Mbox.hs
@@ -10,9 +10,20 @@
 --
 --------------------------------------------------------------------
 
-module Mbox (Mbox(..), MboxMessage(..)) where
+module Mbox (Mbox(..), MboxMessage(..), mboxMsgSenderA, mboxMsgTimeA, mboxMsgBodyA) where
+
+import Data.Accessor
 
 newtype Mbox s = Mbox { unMbox :: [MboxMessage s] }
   deriving (Eq, Ord, Show)
 data MboxMessage s = MboxMessage { mboxMsgSender :: s, mboxMsgTime :: s, mboxMsgBody :: s }
   deriving (Eq, Ord, Show)
+
+mboxMsgSenderA :: Accessor (MboxMessage s) s
+mboxMsgSenderA = accessor mboxMsgSender (\x r -> r{mboxMsgSender=x})
+
+mboxMsgTimeA :: Accessor (MboxMessage s) s
+mboxMsgTimeA = accessor mboxMsgTime (\x r -> r{mboxMsgTime=x})
+
+mboxMsgBodyA :: Accessor (MboxMessage s) s
+mboxMsgBodyA = accessor mboxMsgBody (\x r -> r{mboxMsgBody=x})
