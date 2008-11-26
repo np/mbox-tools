8557b79521b1f5473f74ba65c3e7b9411712d84d
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2008-11-26 02:31:51 +0200

    mbox-counting: inform on stderr

--- mbox-counting.hs
+++ mbox-counting.hs
@@ -1,6 +1,7 @@
 import Mbox (unMbox,parseMbox)
 import qualified Data.ByteString.Lazy as B
+import System.IO (hPutStrLn, stderr)
 
 main :: IO ()
-main = do putStrLn "Reading from stdin..."
+main = do hPutStrLn stderr "Reading from stdin..."
           (print . length . unMbox . parseMbox) =<< B.getContents
