2cb7655cef863886063d115f236fb090ee2327b5
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2010-02-21 18:04:02 +0200

    mbox-list: add --take/--drop options

--- mbox-list.hs
+++ mbox-list.hs
@@ -20,38 +20,56 @@ import System.Environment (getArgs)
 import System.Console.GetOpt
 import Data.Record.Label
 
-data Settings = Settings { _fmt  :: ShowFormat
-                         , _dir  :: Direction
-                         , _help :: Bool
+data Settings = Settings { _fmt      :: ShowFormat
+                         , _dir      :: Direction
+                         , _takeOpt  :: Maybe Int
+                         , _dropOpt  :: Maybe Int
+                         , _help     :: Bool
                          }
 $(mkLabels [''Settings])
-help :: Settings :-> Bool
-dir  :: Settings :-> Direction
-fmt  :: Settings :-> ShowFormat
+help     :: Settings :-> Bool
+dir      :: Settings :-> Direction
+takeOpt  :: Settings :-> Maybe Int
+dropOpt  :: Settings :-> Maybe Int
+fmt      :: Settings :-> ShowFormat
 
 
 type Flag = Settings -> Settings
 
 listMbox :: Settings -> [String] -> IO ()
 listMbox opts mboxfiles =
-  mapM_ (putEmails (get fmt opts) . map ((readEmail . mboxMsgBody) &&& id) . mboxMessages)
+  mapM_ (putEmails (get fmt opts) .
+         map ((readEmail . mboxMsgBody) &&& id) .
+         maybe id take (get takeOpt opts) .
+         maybe id drop (get dropOpt opts) .
+         mboxMessages)
     =<< parseMboxFiles (get dir opts) mboxfiles
 
 defaultSettings :: Settings
-defaultSettings = Settings { _fmt  = defaultShowFormat
-                           , _dir  = Forward
-                           , _help = False
+defaultSettings = Settings { _fmt      = defaultShowFormat
+                           , _dir      = Forward
+                           , _takeOpt  = Nothing
+                           , _dropOpt  = Nothing
+                           , _help     = False
                            }
 
 usage :: String -> a
 usage msg = error $ unlines [msg, usageInfo header options, showFormatsDoc]
   where header = "Usage: mbox-list [OPTION] <mbox-file>*"
 
+maybeIntArg :: (Settings :-> Maybe Int) -> ArgDescr (Settings -> Settings)
+maybeIntArg l = ReqArg (set l . Just . read) "NUM"
+
+-- Since
+--   ∀ k1 k2 Positives, take k1 . drop k2 == drop k2 . take (k2 + k1)
+-- one fix an ordering: drop then take.
 options :: [OptDescr Flag]
 options =
   [ fmtOpt usage (set fmt)
-  , Option "r" ["reverse"] (NoArg (mod dir opposite)) "Reverse the mbox order (latest firsts)"
-  , Option "?" ["help"]    (NoArg (set help True)) "Show this help message"
+  , Option "r" ["reverse"]  (NoArg (mod dir opposite))  "Reverse the mbox order (latest firsts)"
+  , Option "d" ["drop"]     (maybeIntArg dropOpt)       "Drop the NUM firsts"
+  , Option "t" ["take"]     (maybeIntArg takeOpt)       "Take the NUM firsts (happens after --drop)"
+  , Option "?" ["help"]     (NoArg (set help True))     "Show this help message"
   ]
 
 main :: IO ()
